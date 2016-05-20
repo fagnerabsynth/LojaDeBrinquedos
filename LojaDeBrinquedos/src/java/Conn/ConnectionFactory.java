75394d05aa21f07d04ca2c27801a8d66bce703cb
fagner <fagner@cpd.sindicato.com.br>
2016-05-20 10:59:12 -0300

    Inclusão do BrinquedoMOD e mais o CRUD!

--- LojaDeBrinquedos/src/java/Conn/ConnectionFactory.java
+++ LojaDeBrinquedos/src/java/Conn/ConnectionFactory.java
@@ -4,20 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
-
 public class ConnectionFactory {
-	public static Connection getConnection() {             
-        try {             
-        	
+
+    private static String bancodedados, usuario, senha, host;
+
+    public static Connection getConnection() {
+       
+        
+        //registra o usuário e a senha e a base de dados
+        bancodedados = "projetoalmir";
+        usuario = "projetoalmir";
+        senha = "123456";
+        host = "localhost";
+        
+        try {
+
             Class.forName("com.mysql.jdbc.Driver");
-            
+
             //configura os parametros de conexao
-            return DriverManager.getConnection ("jdbc:mysql://localhost:3306/projetoalmir", "projetoalmir", "123456");
-            
-           //Processa e retorna a conexão 
-        } catch(ClassNotFoundException  | SQLException e) {             
+            return DriverManager.getConnection("jdbc:mysql://"+host+":3306/" + bancodedados, usuario, senha);
+
+            //Processa e retorna a conexão 
+        } catch (ClassNotFoundException | SQLException e) {
             return null;
-        }             
+        }
     }
 }
