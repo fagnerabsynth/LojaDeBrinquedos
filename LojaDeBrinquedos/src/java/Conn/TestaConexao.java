021f5941acaa7b666c6d59a97c43403bfcf477e0
fagner <>
2016-05-20 12:10:56 -0300

    Crud completo com todas as opções necessárias para o projeto!

--- LojaDeBrinquedos/src/java/Conn/TestaConexao.java
+++ LojaDeBrinquedos/src/java/Conn/TestaConexao.java
@@ -1,6 +1,5 @@
 package Conn;
 import Models.BrinquedoMOD;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -8,11 +7,19 @@ import java.util.ArrayList;
 public class TestaConexao {
 	 public static void main(String[] args) throws SQLException{
      
-             BrinquedoMOD i = new BrinquedoMOD();
-             i.codigo = 1;
+             BrinquedoMOD c = new BrinquedoMOD();
+             c.categoria="teste";
+             c.descricao="teste2";
+             c.detalhe="teste3";
+             c.imagem="teste4";
+             c.marca="teste5";
+             c.preco=15.50;
+             
              Database b = new Database();
-              b.delete(i);
-        
+             
+             b.insert(c);
+             ArrayList<BrinquedoMOD> d = b.select();
+             ArrayList<BrinquedoMOD> i = d ;
              
 	    }
 }
